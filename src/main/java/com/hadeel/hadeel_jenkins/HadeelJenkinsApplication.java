4a286290cd7c8c1eb973be4aa533681244112a5f
Hadeel Baghdadi <hadeelbaghdadi236@nhitm.ac.in>
2026-03-01 23:23:53 +0530

    Uploading Spring Boot project

--- src/main/java/com/hadeel/hadeel_jenkins/HadeelJenkinsApplication.java
+++ src/main/java/com/hadeel/hadeel_jenkins/HadeelJenkinsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HadeelJenkinsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HadeelJenkinsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HadeelJenkinsApplication.class, args);
+    }
 }
