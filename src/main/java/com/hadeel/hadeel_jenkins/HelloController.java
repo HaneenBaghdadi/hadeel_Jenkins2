c55e275b0f30563dcab3388cde26dc31cd4f0737
HaneenBaghdadi <2025.haneen.baghdadi@ves.ac.in>
2026-03-01 23:56:22 +0530

    Update HelloController.java

--- src/main/java/com/hadeel/hadeel_jenkins/HelloController.java
+++ src/main/java/com/hadeel/hadeel_jenkins/HelloController.java
@@ -1,4 +1,4 @@
-package com.hadeel.hadeel_jenkins;
+package com.hadeel.hadeeljenkins;  // ← remove the underscore
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Jenkins CI/CD 🚀";
+        return "Hello from Jenkins CI/CD!";
     }
 }
