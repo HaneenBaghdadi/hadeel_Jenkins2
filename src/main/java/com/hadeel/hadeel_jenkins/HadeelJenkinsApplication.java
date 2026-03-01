cc30599a79330ec5999ccfb5e37aac50e4fd20d5
HaneenBaghdadi <2025.haneen.baghdadi@ves.ac.in>
2026-03-01 23:00:02 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/hadeel/hadeel_jenkins/HadeelJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.hadeel.hadeel_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadeelJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadeelJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hadeel/hadeel_jenkins/HadeelJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadeel.hadeel_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadeelJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
